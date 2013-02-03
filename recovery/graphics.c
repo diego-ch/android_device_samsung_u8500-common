22298f6b833a8a184f641ade356d5ca28b239fe3
Diego Porto <diego.cfporto@gmail.com>
2013-02-03 11:43:39 -0300

    recovery: cleanup init and add graphics.c

--- recovery/graphics.c
+++ recovery/graphics.c
@@ -38,8 +38,18 @@
 
 #include "minui.h"
 
+#if defined(RECOVERY_BGRA)
 #define PIXEL_FORMAT GGL_PIXEL_FORMAT_BGRA_8888
 #define PIXEL_SIZE   4
+#elif defined(RECOVERY_RGBX)
+#define PIXEL_FORMAT GGL_PIXEL_FORMAT_RGBX_8888
+#define PIXEL_SIZE   4
+#else
+#define PIXEL_FORMAT GGL_PIXEL_FORMAT_RGB_565
+#define PIXEL_SIZE   2
+#endif
+
+#define NUM_BUFFERS 2
 
 typedef struct {
     GGLSurface texture;
@@ -51,9 +61,10 @@ typedef struct {
 static GRFont *gr_font = 0;
 static GGLContext *gr_context = 0;
 static GGLSurface gr_font_texture;
-static GGLSurface gr_framebuffer[2];
+static GGLSurface gr_framebuffer[NUM_BUFFERS];
 static GGLSurface gr_mem_surface;
 static unsigned gr_active_fb = 0;
+static unsigned double_buffering = 0;
 
 static int gr_fb_fd = -1;
 static int gr_vt_fd = -1;
@@ -136,6 +147,12 @@ static int get_framebuffer(GGLSurface *fb)
 
     fb++;
 
+    /* check if we can use double buffering */
+    if (vi.yres * fi.line_length * 2 > fi.smem_len)
+        return fd;
+
+    double_buffering = 1;
+
     fb->version = sizeof(*fb);
     fb->width = vi.xres;
     fb->height = vi.yres;
@@ -158,8 +175,8 @@ static void get_memory_surface(GGLSurface* ms) {
 
 static void set_active_framebuffer(unsigned n)
 {
-    if (n > 1) return;
-    vi.yres_virtual = vi.yres * PIXEL_SIZE;
+    if (n > 1 || !double_buffering) return;
+    vi.yres_virtual = vi.yres * NUM_BUFFERS;
     vi.yoffset = n * vi.yres;
     vi.bits_per_pixel = PIXEL_SIZE * 8;
     if (ioctl(gr_fb_fd, FBIOPUT_VSCREENINFO, &vi) < 0) {
@@ -172,7 +189,8 @@ void gr_flip(void)
     GGLContext *gl = gr_context;
 
     /* swap front and back buffers */
-    gr_active_fb = (gr_active_fb + 1) & 1;
+    if (double_buffering)
+        gr_active_fb = (gr_active_fb + 1) & 1;
 
     /* copy data from the in-memory surface to the buffer we're about
      * to make active. */
@@ -296,7 +314,7 @@ static void gr_init_font(void)
     gr_font->ascent = font.cheight - 2;
 }
 
-int gr_init(void)
+int gr_init_real(void)
 {
     gglInit(&gr_context);
     GGLContext *gl = gr_context;
@@ -336,6 +354,13 @@ int gr_init(void)
     gr_fb_blank(true);
     gr_fb_blank(false);
 
+    return 0;
+}
+
+int gr_init(void)
+{
+    gr_init_real();
+    gr_init_real();
     return 0;
 }
 
