39e2097dc56188e1fa7bf406915eaf77503641ce
Shantanu Londhe <shantanulondhe26@gmail.com>
2013-06-22 16:23:48 +0530

    bt : typo
    Typos. Typos everywhere..
    :p

--- common.mk
+++ common.mk
@@ -86,8 +86,8 @@ PRODUCT_COPY_FILES += \
 
 # BT
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/bluetooth/01bt/:system/etc/init.d/01bt \
-    $(COMMON_PATH)/bluetooth/bt_vendor.conf/:system/etc/bluetooth/bt_vendor.conf
+    $(COMMON_PATH)/bluetooth/01bt:system/etc/init.d/01bt \
+    $(COMMON_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 PRODUCT_PACKAGES += \
     brcm_patchram_plus
 
