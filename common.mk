6c6896d214ac50e933d0155dcf3397b7cbcc5a3e
Shantanu Londhe <shantanulondhe26@gmail.com>
2013-06-22 16:18:20 +0530

    02omxsym : typo

--- common.mk
+++ common.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/02omxsym/:system/etc/init.d/02omxsym \
+    $(COMMON_PATH)/configs/02omxsym:system/etc/init.d/02omxsym \
     $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
