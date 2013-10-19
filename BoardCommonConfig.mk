42566ef3ad504c246009f023c930ef42231d7a35
Rox-Ace2 <rooobertinhooo@gmail.com>
2013-10-19 23:15:29 +0200

    Fix typo

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -89,7 +89,7 @@ BOARD_CHARGER_RES := device/samsung/u8500-common/res/charger
 
 # Recovery
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/u8500-common/recovery/graphics.c
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../../device/samsung/u8500-common/recovery/recovery_keys.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/u8500-common/recovery/recovery_keys.c
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
