5573527e501b9cde769da31a2a75dd0dab76c381
Franco Rapetti <frapeti@gmail.com>
2013-10-24 16:17:50 -0200

    copying ble main.conf

--- common.mk
+++ common.mk
@@ -98,6 +98,10 @@ PRODUCT_PACKAGES += \
     VisualizationWallpapers \
     librs_jni
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
