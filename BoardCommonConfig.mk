84eebc123ff9785b7ec74a15b34f0149b049cb90
Franco Rapetti <frapeti@gmail.com>
2013-11-20 19:32:07 -0300

    disable custom releasetools

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -121,6 +121,6 @@ BOARD_RECOVERY_SWIPE := true
 # Misc
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
-# Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/u8500-common/releasetools/u8500_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/u8500-common/releasetools/u8500_img_from_target_files
+# Custom releasetools - disabled
+# TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/u8500-common/releasetools/u8500_ota_from_target_files
+# TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/u8500-common/releasetools/u8500_img_from_target_files
