d45b13780301a1efb5f615e27704a36f8091e5d7
Robert Rozic <rooobertinhooo@gmail.com>
2013-12-03 17:26:27 +0100

    Add more lines to common.mk

--- common.mk
+++ common.mk
@@ -17,6 +17,10 @@ COMMON_PATH := device/samsung/u8500-common
 
 DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
+# Our devices are HDPI
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
 # Packages
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
@@ -29,6 +33,7 @@ PRODUCT_PACKAGES += \
     power.montblanc \
     lights.montblanc \
     SamsungServiceMode \
+	CMAccount \
     Torch
 
 # Init files
