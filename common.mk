b378c24a1b0bba817e53c09e6c37b7bf4c1fb6e9
Robert Rozic <rooobertinhooo@gmail.com>
2014-06-05 20:42:48 +0200

    Move more lines to common init
    ${ro.hardware} will be replaced with system property for device ( etc. samsungcodina,samsungjanice... )
    +Make common usb init

--- common.mk
+++ common.mk
@@ -27,7 +27,8 @@ PRODUCT_PACKAGES += \
 
 # U8500 Common init
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/init.u8500.rc:root/init.u8500.rc
+    $(COMMON_PATH)/rootdir/init.u8500.rc:root/init.u8500.rc \
+    $(COMMON_PATH)/rootdir/init.u8500.usb.rc:root/init.u8500.usb.rc
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
